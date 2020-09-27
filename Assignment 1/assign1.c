3a47ba034886b78ffe9af62f35f8d98dd0a89014
yunfan.yang1 <cloudyyoung74@outlook.com>
2020-09-27 17:42:43 -0600

    Done most

--- Assignment 1/assign1.c
+++ Assignment 1/assign1.c
@@ -9,26 +9,22 @@
 #define max(x, y) (x > y) ? x : y
 #define min(x, y) (x < y) ? x : y
 
-struct Table
-{
+struct Table {
     int array[20][20];
     int row;
     int column;
 };
 
-struct WordFrequency
-{
+struct WordFrequency {
     double frequency;
     int word;
     int times;
     int document;
 };
 
-int randomNum(int m, int n)
-{
+int randomNum(int m, int n) {
     // If the upper bound and the lower bound are the same
-    if (m == n)
-    {
+    if (m == n) {
         return m;
     }
 
@@ -39,70 +35,67 @@ int randomNum(int m, int n)
     return rand() % (upper - lower) + lower;
 }
 
-void initialize(struct Table *table, char *file)
-{
+void initialize(struct Table* table, char* file) {
     bool fromFile = (file != NULL);
-    FILE *fp;
+    FILE* fp;
     char text[UCHAR_MAX];
 
-    if (fromFile)
+    if (fromFile) {
         fp = fopen(file, "r");
+    }
 
-    for (int t = 0; t < table->row; t++)
-    {
+    for (int t = 0; t < table->row; t++) {
         if (fromFile && fgets(text, sizeof(text), fp) == NULL)
             break;
 
-        for (int r = 0; r < table->column; r++)
-        {
-            if (fromFile)
-            {
+        for (int r = 0; r < table->column; r++) {
+            if (fromFile) {
                 int num = text[r * 2];
 
                 if (num >= 48 && num <= 57)
                 {
                     table->array[t][r] = num - 48;
-                }
-                else
+                } else
                 {
                     break;
                 }
-            }
-            else
-            {
+            } else {
                 int rand = randomNum(0, 9);
                 table->array[t][r] = rand;
             }
         }
     }
 
-    if (fromFile)
+    if (fromFile) {
         fclose(fp);
+    }
+
 }
 
-void display(struct Table *table)
-{
+void display(struct Table* table) {
     printf("===== Table ===== \n");
-    for (int t = 0; t < table->row; t++)
-    {
-        for (int r = 0; r < table->column; r++)
-        {
+    for (int t = 0; t < table->row; t++) {
+        for (int r = 0; r < table->column; r++) {
             printf(" %d ", table->array[t][r]);
         }
         printf("\n");
     }
 }
 
-struct WordFrequency *topRelevantDocs(struct Table *table, int index)
-{
-    struct WordFrequency word;
+void topRelevantDocs(struct Table* table, int index, int top) {
 
-    struct WordFrequency *words = (struct WordFrequency *)calloc(table->row, sizeof(word));
-    for (int t = 0; t < table->row; t++)
-    {
+    // Preventing invalid user input. Index cannot be greater than the table size.
+    index = min(index, table->column - 1);
+    index = max(0, index);
+    top = min(top, table->row);
+    top = max(0, top);
+
+    struct WordFrequency words[20];
+    for (int t = 0; t < table->row; t++) {
         int documentSize = 0;
-        for (int r = 0; r < table->column; r++)
+        for (int r = 0; r < table->column; r++) {
             documentSize += table->array[t][r];
+        }
 
         struct WordFrequency wf;
         wf.document = t;
@@ -113,12 +106,9 @@ struct WordFrequency *topRelevantDocs(struct Table *table, int index)
     }
 
     // Bubble Sort
-    for (int t = 0; t < table->row; t++)
-    {
-        for (int r = 0; r < table->row - 1; r++)
-        {
-            if (words[r].frequency < words[r + 1].frequency)
-            {
+    for (int t = 0; t < table->row; t++) {
+        for (int r = 0; r < table->row - 1; r++) {
+            if (words[r].frequency < words[r + 1].frequency) {
                 struct WordFrequency wf = words[r];
                 words[r] = words[r + 1];
                 words[r + 1] = wf;
@@ -126,46 +116,50 @@ struct WordFrequency *topRelevantDocs(struct Table *table, int index)
         }
     }
 
-    return words;
+
+    printf("The top documents are: \n");
+    for (int t = 0; t < top; t++)
+    {
+        // printf("Word %d in ", words[t].word);
+        printf("Document %d: ", words[t].document);
+        printf("Times of %d and ", words[t].times);
+        printf("Frequency of %.1f%% ", words[t].frequency * 100);
+        printf("\n");
+    }
+
 }
 
-void logToFile()
-{
+void logToFile() {
     freopen("assign1.log", "wt", stdout);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
 
     time_t timestamp;
     srand((unsigned)time(&timestamp));
 
-    // logToFile();
-
     int row, column;
 
-    if (argc >= 3)
-    {
+    if (argc >= 3) {
         row = atoi(argv[1]), column = atoi(argv[2]);
         row = min(max(5, row), 20), column = min(max(5, column), 20);
-    }else{
+    } else {
         row = column = 5;
     }
-    
 
     struct Table table;
     table.row = row;
     table.column = column;
 
-    initialize(&table, argv[3]);
+    initialize(&table, (argc >= 4) ? argv[3] : NULL);
     display(&table);
 
     printf("\n");
 
     char command = 'y';
 
-    do
-    {
+    do {
+
         int index, top;
 
         printf("What is the index of the word you are searching for? ");
@@ -176,28 +170,7 @@ int main(int argc, char *argv[])
 
         printf("\n");
 
-        // Preventing invalid user input. Index cannot be greater than the table size.
-        index = min(index, table.column - 1);
-        index = max(0, index);
-
-        // Top words
-        struct WordFrequency *topWords = (struct WordFrequency *)topRelevantDocs(&table, index);
-
-        int size = 0;
-        size = min(top, table.row);
-        size = max(0, size);
-
-        printf("The top documents are: \n");
-        for (int t = 0; t < size; t++)
-        {
-            // printf("Word %d in ", topWords[t].word);
-            printf("Document %d: ", topWords[t].document);
-            printf("Times of %d and ", topWords[t].times);
-            printf("Frequency of %.1f%% ", topWords[t].frequency * 100);
-            printf("\n");
-        }
-
-        free(topWords);
+        topRelevantDocs(&table, index, top);
 
         printf("\n");
 
