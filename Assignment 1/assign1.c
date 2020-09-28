d9777bef8dd24fed94d45937d5feee042f2c0d89
yunfan.yang1 <cloudyyoung74@outlook.com>
2020-09-27 21:32:17 -0600

    Finalized

--- Assignment 1/assign1.c
+++ Assignment 1/assign1.c
@@ -11,11 +11,17 @@
 #define max(x, y) (x > y) ? x : y
 #define min(x, y) (x < y) ? x : y
 
+// Constants, avoiding magic numbers
+#define MAX_ROW 20
+#define MAX_COL 20
+#define MIN_ROW 5
+#define MIN_COL 5
+
 // Log file stream
 FILE* fp_log;
 
 struct Table {
-    int array[20][20];
+    int array[MAX_ROW][MAX_COL];
     int row;
     int column;
 };
@@ -42,15 +48,17 @@ int randomNum(int m, int n) {
 }
 
 
-
+// Opening the logging
 void logToFile() {
     fp_log = fopen("assign1.log", "w");
 }
 
+// Closing the logging
 void logOffFile() {
     fclose(fp_log);
 }
 
+// Customized print. Same usage with printf, print to console and log file at the same time.
 void print(const char* restrict format, ...) {
     va_list vlist;
 
@@ -63,6 +71,7 @@ void print(const char* restrict format, ...) {
     va_end(vlist);
 }
 
+// Customized scan. Same usage with scanf, scan to console and log file at the same time.
 void scan(const char* restrict format, ...) {
     va_list vlist;
 
@@ -89,9 +98,9 @@ void scan(const char* restrict format, ...) {
     va_end(vlist);
 }
 
-
+// Initializing and populating the table
 void initialize(struct Table* table, char* file) {
-    bool fromFile = (file != NULL);
+    bool fromFile = (file != NULL); // Whether populate table from file
     FILE* fp;
     char text[UCHAR_MAX];
 
@@ -99,23 +108,26 @@ void initialize(struct Table* table, char* file) {
         fp = fopen(file, "r");
     }
 
+    // Populate table with default value 0
     for (int k = 0; k < 20; k++) {
         for (int j = 0; j < 20; j++) {
             table->array[k][j] = 0;
         }
     }
 
+    // Populate table with actual values
     for (int t = 0; t < table->row; t++) {
+        // Read next line of the file
         if (fromFile && fgets(text, sizeof(text), fp) == NULL) {
             break;
         }
 
         for (int r = 0; r < table->column; r++) {
             if (fromFile) {
-                int num = text[r * 2];
+                int num = text[r * 2]; // Next character
 
                 if (num >= 48 && num <= 57) {
-                    table->array[t][r] = num - 48;
+                    table->array[t][r] = (num - 48);
                 } else {
                     break;
                 }
@@ -132,6 +144,7 @@ void initialize(struct Table* table, char* file) {
 
 }
 
+// Display the table in console
 void display(struct Table* table) {
     print("===== Table =====\n");
     for (int t = 0; t < table->row; t++) {
@@ -142,17 +155,19 @@ void display(struct Table* table) {
     }
 }
 
+// Show top relevant documents
 void topRelevantDocs(struct Table* table, int index, int top) {
 
-    // Preventing invalid user input. Index cannot be greater than the table size.
+    // Preventing invalid user input. Index cannot be greater than the table size or smaller than 0.
+    // If smaller than 0, set to 0. If greater than table size, set to table size.
     index = min(index, table->column - 1);
     index = max(0, index);
     top = min(top, table->row);
     top = max(0, top);
 
-    struct WordFrequency words[20];
+    struct WordFrequency words[MAX_ROW];
     for (int t = 0; t < table->row; t++) {
-        int documentSize = 0;
+        int documentSize = 0; // Total words of current document
         for (int r = 0; r < table->column; r++) {
             documentSize += table->array[t][r];
         }
@@ -169,6 +184,7 @@ void topRelevantDocs(struct Table* table, int index, int top) {
     for (int t = 0; t < table->row; t++) {
         for (int r = 0; r < table->row - 1; r++) {
             if (words[r].frequency < words[r + 1].frequency) {
+                // Swap wordFrequency
                 struct WordFrequency wf = words[r];
                 words[r] = words[r + 1];
                 words[r + 1] = wf;
@@ -176,11 +192,11 @@ void topRelevantDocs(struct Table* table, int index, int top) {
         }
     }
 
-
+    // Print out the result
     print("The top documents are: \n");
     for (int t = 0; t < top; t++)
     {
-        // print("Word %02d in ", words[t].word);
+        // print("Word %02d in ", words[t].word); // Uncomment if neccessary
         print("Document %02d: ", words[t].document);
         print("Occurence of %d and ", words[t].times);
         print("Frequency of %.1f%% ", words[t].frequency * 100);
@@ -189,34 +205,35 @@ void topRelevantDocs(struct Table* table, int index, int top) {
 
 }
 
+
 int main(int argc, char* argv[]) {
 
     time_t timestamp;
     srand((unsigned)time(&timestamp));
 
-    int row, column;
+    logToFile(); // Initializing logging
 
-    if (argc >= 3) {
+    int row, column;
+    if (argc >= 3) { // If row and column are given in the command line
         row = atoi(argv[1]), column = atoi(argv[2]);
-        row = min(max(5, row), 20), column = min(max(5, column), 20);
-    } else {
-        row = column = 5;
+        row = min(max(MIN_ROW, row), MAX_ROW), column = min(max(MIN_COL, column), MAX_COL);
+    } else { // Otherwise, set to the minimum
+        row = column = MIN_ROW;
     }
 
+    // Initializing table
     struct Table table;
     table.row = row;
     table.column = column;
 
-    logToFile();
-
-    initialize(&table, (argc >= 4) ? argv[3] : NULL);
+    // Populate and display
+    initialize(&table, (argc >= 4) ? argv[3] : NULL); // If file is given, otherwise NULL
     display(&table);
-
 
     print("\n");
 
+    // User command
     char command;
-
     do {
 
         int index, top;
@@ -229,10 +246,12 @@ int main(int argc, char* argv[]) {
 
         print("\n");
 
-        topRelevantDocs(&table, index, top);
 
+        // Search top documents
+        topRelevantDocs(&table, index, top);
         print("\n");
 
+        // Ask for another search
         print("Do you want to search again? (y/n) ");
         scan(" %c", &command);
 
@@ -243,6 +262,7 @@ int main(int argc, char* argv[]) {
 
     print("Ended.\n");
 
+    // Closing logging
     logOffFile();
 
     return 0;
