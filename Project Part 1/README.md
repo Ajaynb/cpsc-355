76426e8859a50c85cb4e7d2e47c8a58f6b5641c6
Cloudy Young <cloudyyoung74@outlook.com>
2020-10-16 22:48:41 +0800

    Finish bomb

---
+++ Project Part 1/README.md
@@ -0,0 +1,2 @@
+http://ascii-table.com/ansi-escape-sequences.php
+- https://en.cppreference.com/w/c/io/vfscanf
--- Project Part 1/project.c
+++ Project Part 1/project.c
@@ -10,6 +10,7 @@
 // Determine which one is greater/smaller between the given two numbers
 #define max(x, y) (x > y) ? x : y
 #define min(x, y) (x < y) ? x : y
+#define clear_screen() printf("\033[2J");
 
 #define MIN_ROW 10
 #define MIN_COL 10
@@ -23,9 +24,11 @@ struct Board {
     int row;
     int column;
     int size;
-    int score;
-    unsigned int negAmount;
-    unsigned int speAmount;
+    unsigned int negatives;
+    unsigned int specials;
+    int lives;
+    float score;
+    int bombs;
 };
 
 struct Block {
@@ -61,16 +64,19 @@ int randomNum(int m, int n) {
 
 void initializeGame(struct Board* board) {
     // Ensure the board is at least the minimum size
-    board->row = max(MIN_ROW, board->row);
-    board->column = max(MIN_COL, board->column);
+    // board->row = max(MIN_ROW, board->row);
+    // board->column = max(MIN_COL, board->column);
 
     // Intialize map by allocating memory
     board->size = board->row * board->column;
     board->array = calloc(sizeof(struct Block), board->size);
 
     // Initialize statistics
-    board->negAmount = 0;
-    board->speAmount = 0;
+    board->negatives = 0;
+    board->specials = 0;
+    board->lives = 3;
+    board->score = 0.0;
+    board->bombs = 3;
 
     // Generate map
     for (int t = 0; t < board->size; t++) {
@@ -87,19 +93,19 @@ void initializeGame(struct Board* board) {
     }
     
     // Flip to negative numbers
-    while (board->negAmount < (int)(board->size * NEG_PERCENT)) {
+    while (board->negatives < (int)(board->size * NEG_PERCENT)) {
         int index = randomNum(0, board->size - 1);
         if (board->array[index].value >= 0) {
             board->array[index].value *= -1;
-            board->negAmount++;
+            board->negatives++;
         }
     }
 
     // Flip to specials
-    while (board->speAmount < (int)(board->size * SPE_PERCENT)) {
+    while (board->specials < (int)(board->size * SPE_PERCENT)) {
         int index = randomNum(0, board->size - 1);
         board->array[index].value = REWARD;
-        board->speAmount++;
+        board->specials++;
     }
 
     // Generate Exit
@@ -107,7 +113,7 @@ void initializeGame(struct Board* board) {
     board->array[exitIndex].value = EXIT;
 }
 
-void displayBoard(struct Board* board, bool peek) {
+void displayGame(struct Board* board, bool peek) {
     for (int t = 0; t < board->size; t++) {
         int col = t % board->column;
         int value = board->array[t].value;
@@ -117,49 +123,94 @@ void displayBoard(struct Board* board, bool peek) {
             case REWARD:
             case EXIT:
                 if (peek) printf("   %c    ", value);
-                else printf(" %c ", value);
+                else if (value == EXIT) printf("\033[1;34m%c\033[0;m  ", value);
+                else printf("\033[1;36m%c\033[0;m  ", value);
                 break;
             default:
                 if (peek) printf("%6.2f  ", board->array[t].value);
-                else printf(" %c ", (board->array[t].value > 0) ? '+' : '-');
+                else printf("%s  ", (board->array[t].value > 0) ? "\033[1;32m+\033[0m" : "\033[1;31m-\033[0m");
                 break;
             }
         } else {
-            printf(" X ");
+            printf("x  ");
         }
-        
 
         if (col == board->column - 1) {
             printf("\n");
         }
     }
 
-    printf("negAmount %d, ", board->negAmount);
-    printf("size %d\n", board->size);
-    float negRate = (1.0 * board->negAmount / board->size) * 100;
-    printf("Total negative number rate: %.2f%%\n", negRate);
+    if (peek) {
+        float negRate = (1.0 * board->negatives / board->size) * 100;
+        printf("Total negatives: %d/%d (%.2f%%)\n", board->negatives, board->size, negRate);
 
-    printf("speAmount %d, ", board->speAmount);
-    printf("size %d\n", board->size);
-    float speRate = (1.0 * board->speAmount / board->size) * 100;
-    printf("Total specials rate: %.2f%%\n", speRate);
+        float speRate = (1.0 * board->specials / board->size) * 100;
+        printf("Total specials:  %d/%d (%.2f%%)\n", board->specials, board->size, speRate);
+    } else {
+        printf("Lives: %d\nScore: %.2f\nBombs: %d\n", board->lives, board->score, board->bombs);
+    }
+
 }
 
+void playGame(struct Board* board, const int x, const int y) {
+    for (int t = -1; t <= 1; t++) {
+        for (int r = -1; r <= 1; r++) {
+            int index = ((x + t) * board->column) + y + r;
+            int current_y = index % board->column;
+
+            if (index >= 0 && index < board->size && current_y >= y - 1 && current_y <= y + 1) {
+                board->array[index].covered = false;
+            }
+        }
+    }
+}
+
+
+int extractInput(const char* buf, const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int rc = vsscanf(buf, fmt, ap);
+    va_end(ap);
+    return rc;
+}
 
 int main(int argc, char* argv[]) {
 
     time_t timestamp;
     srand((unsigned)time(&timestamp));
 
+    clear_screen();
+
     struct Board board;
-    board.row = 27;
-    board.column = 13;
+    board.row = 5;
+    board.column = 5;
 
+    printf("Board: \n");
+    
     initializeGame(&board);
-    displayBoard(&board, true);
+    displayGame(&board, true);
 
-    displayBoard(&board, false);
+    printf("\nPress ENTER key to start game...");
+    getchar();
 
+    char input[10];
+    int x = -10, y = -10;
 
+    while (input[0] != 'q') {
+        clear_screen();
+
+        if (extractInput(input, "%d %d", &x, &y) == 2) {
+            playGame(&board, x, y);
+        }
+
+        displayGame(&board, false);
+
+        printf("\n");
+        printf("Enter q to quit, \n");
+        printf("Enter bomb position (x y): ");
+        gets(input);
+        printf("\n");
+    }
 
 }
