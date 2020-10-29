002041e243f7b666d763d475d5265de01372a952
yunfan.yang1 <cloudyyoung74@outlook.com>
2020-10-29 09:56:33 -0600

    Add set random seed

--- Assignment 3/assign3.asm
+++ Assignment 3/assign3.asm
@@ -1,12 +1,19 @@
 
-
+define(x_multiplier, x19)
+define(x_multiplicand, x20)
 
 main:                                                               // main function
         // Saves state
         stp     x29,    x30,    [sp, -16]!                          // space stack pointer
         mov     x29,    sp                                          // frame-pointer = stack-pointer;
 
+        // Set random seed
+        mov     x0,     0                       // 1st parameter: 0
+        bl      time                            // time(0);
+        bl      srand                           // srand(time(0)); set random seed to current timestamp
 
+multiply:
+        
 
 
         // Restores state
